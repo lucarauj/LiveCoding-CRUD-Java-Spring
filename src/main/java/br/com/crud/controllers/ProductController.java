71e94304a4a538b66b9014c5e4e837a94c2176ef
lucarauj <lukas.martins193@gmail.com>
2023-06-10 16:45:04 -0300

    Update metodo updateProduct no ProductController

--- src/main/java/br/com/crud/controllers/ProductController.java
+++ src/main/java/br/com/crud/controllers/ProductController.java
@@ -6,7 +6,10 @@ import br.com.crud.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -28,14 +31,21 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
+    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid ProductDto data, @PathVariable String id) {
-        Product product = productRepository.getReferenceById(id);
-        product.setName(data.name());
-        product.setPrice_in_cents(data.price_in_cents());
-        return ResponseEntity.ok(productRepository.save(product));
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if(optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{id}")
+    @Transactional
     public ResponseEntity deleteProduct(@PathVariable String id) {
         productRepository.deleteById(id);
         return ResponseEntity.ok().build();
